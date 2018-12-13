afae7b2795cb21b4005f0d4779e9fe80112476f3
dmitriy.sinichkin <dmitriy.sinichkin@gmail.com>
2018-12-13 12:32:13 +0300

    add constants

---
+++ pkg/constanst.go
@@ -0,0 +1,9 @@
+package pkg
+
+const (
+	PayOneMicroserviceVersion = "latest"
+
+	PayOneSubscriberNotifierName = "go.p1.payone.notifier-subscriber"
+
+	PayOneTopicNotifyPaymentName = "payone.topic.notify-payment"
+)
