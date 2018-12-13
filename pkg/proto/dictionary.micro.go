8ac3c8c06cf6baaf29bfcbc31c3f73cf7feab674
dmitriy.sinichkin <dmitriy.sinichkin@gmail.com>
2018-12-13 21:16:51 +0300

    updated project structures

--- pkg/proto/dictionary.micro.go
+++ pkg/proto/dictionary.micro.go
@@ -2,7 +2,7 @@
 // source: dictionary.proto
 
 /*
-Package processing is a generated protocol buffer package.
+Package proto is a generated protocol buffer package.
 
 It is generated from these files:
 	dictionary.proto
@@ -12,15 +12,15 @@ It has these top-level messages:
 	Country
 	Currency
 */
-package processing
+package proto
 
-import proto "github.com/golang/protobuf/proto"
+import proto1 "github.com/golang/protobuf/proto"
 import fmt "fmt"
 import math "math"
 import _ "github.com/golang/protobuf/ptypes/timestamp"
 
 // Reference imports to suppress errors if they are not otherwise used.
-var _ = proto.Marshal
+var _ = proto1.Marshal
 var _ = fmt.Errorf
 var _ = math.Inf
 
@@ -28,4 +28,4 @@ var _ = math.Inf
 // is compatible with the proto package it is being compiled against.
 // A compilation error at this line likely means your copy of the
 // proto package needs to be updated.
-const _ = proto.ProtoPackageIsVersion2 // please upgrade the proto package
+const _ = proto1.ProtoPackageIsVersion2 // please upgrade the proto package
