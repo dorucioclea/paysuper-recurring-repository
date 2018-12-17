6e403dba7615d184dfc35596a45b89a9ca8d4a51
dmitriy.sinichkin <dmitriy.sinichkin@gmail.com>
2018-12-17 14:25:58 +0300

    PAY-0

--- internal/repository/order.go
+++ internal/repository/order.go
@@ -9,6 +9,6 @@ import (
 
 func (r *Repository) UpdateOrder(ctx context.Context, req *billing.Order, rsp *repository.Response) error {
 	log.Print("Received Repository.UpdateOrder request")
-	rsp.Msg = "Update order with id " + req.Id
+	rsp.Msg = "Update order with id " + string(req.Id)
 	return nil
 }
