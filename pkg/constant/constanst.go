67679e5d0301381781446a8b0307aaf757b4d45a
dmitriy.sinichkin <dmitriy.sinichkin@gmail.com>
2019-01-18 14:25:34 +0300

    PAY-32 delete constant

--- pkg/constant/constanst.go
+++ pkg/constant/constanst.go
@@ -2,8 +2,6 @@ package constant
 
 const (
 	PayOneMicroserviceVersion = "latest"
-
-	PayOneSubscriberNotifierName = "go.p1.payone.notifier-subscriber"
 	PayOneRepositoryServiceName  = "p1payrepository"
 
 	PayOneTopicNotifyPaymentName = "payone.topic.notify-payment"
