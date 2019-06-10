fcb7f837a47610c11a67b9b65364f0ba9404b1c5
dmitriy.sinichkin <dmitriy.sinichkin@gmail.com>
2019-06-10 15:53:15 +0300

    PAY-0 save card holder field

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -1,151 +1,153 @@
 package repository
 
 import (
-    "context"
-    "fmt"
-    "github.com/globalsign/mgo"
-    "github.com/globalsign/mgo/bson"
-    "github.com/golang/protobuf/ptypes"
-    "github.com/paysuper/paysuper-recurring-repository/internal/database"
-    "github.com/paysuper/paysuper-recurring-repository/pkg/constant"
-    "github.com/paysuper/paysuper-recurring-repository/pkg/proto/entity"
-    "github.com/paysuper/paysuper-recurring-repository/pkg/proto/repository"
-    "go.uber.org/zap"
-    "time"
+	"context"
+	"fmt"
+	"github.com/globalsign/mgo"
+	"github.com/globalsign/mgo/bson"
+	"github.com/golang/protobuf/ptypes"
+	"github.com/paysuper/paysuper-recurring-repository/internal/database"
+	"github.com/paysuper/paysuper-recurring-repository/pkg/constant"
+	"github.com/paysuper/paysuper-recurring-repository/pkg/proto/entity"
+	"github.com/paysuper/paysuper-recurring-repository/pkg/proto/repository"
+	"go.uber.org/zap"
+	"time"
 )
 
 const (
-    savedCardQueryErrorFind   = "Query to find saved card failed"
-    savedCardQueryErrorDelete = "Query to delete saved card failed"
-    savedCardQueryErrorCreate = "Query to create saved card failed"
+	savedCardQueryErrorFind   = "Query to find saved card failed"
+	savedCardQueryErrorDelete = "Query to delete saved card failed"
+	savedCardQueryErrorCreate = "Query to create saved card failed"
 )
 
 type Repository struct {
-    db *database.Source
+	db *database.Source
 }
 
 func NewRepositoryService(db *database.Source) *Repository {
-    return &Repository{db: db}
+	return &Repository{db: db}
 }
 
 func (r *Repository) InsertSavedCard(
-    ctx context.Context,
-    req *repository.SavedCardRequest,
-    rsp *repository.Result,
+	ctx context.Context,
+	req *repository.SavedCardRequest,
+	rsp *repository.Result,
 ) error {
-    var card *entity.SavedCard
+	var card *entity.SavedCard
 
-    query := bson.M{"token": req.Token, "masked_pan": req.MaskedPan}
-    err := r.db.Collection(constant.CollectionSavedCard).Find(query).One(&card)
+	query := bson.M{"token": req.Token, "masked_pan": req.MaskedPan}
+	err := r.db.Collection(constant.CollectionSavedCard).Find(query).One(&card)
 
-    if err != nil && err != mgo.ErrNotFound {
-        r.logError(savedCardQueryErrorFind, []interface{}{"error", err.Error(), "query", query})
-        return constant.ErrDatabase
-    }
+	if err != nil && err != mgo.ErrNotFound {
+		r.logError(savedCardQueryErrorFind, []interface{}{"error", err.Error(), "query", query})
+		return constant.ErrDatabase
+	}
 
-    if card != nil {
-        if card.RecurringId == req.RecurringId {
-            return nil
-        } else {
-            card.RecurringId = req.RecurringId
-            card.UpdatedAt = ptypes.TimestampNow()
+	if card != nil {
+		if card.RecurringId == req.RecurringId {
+			return nil
+		} else {
+			card.RecurringId = req.RecurringId
+			card.UpdatedAt = ptypes.TimestampNow()
 
-            err = r.db.Collection(constant.CollectionSavedCard).UpdateId(bson.ObjectIdHex(card.Id), card)
-        }
-    } else {
-        card = &entity.SavedCard{
-            Id:          bson.NewObjectId().Hex(),
-            Token:       req.Token,
-            ProjectId:   req.ProjectId,
-            MerchantId:  req.MerchantId,
-            MaskedPan:   req.MaskedPan,
-            RecurringId: req.RecurringId,
-            Expire:      req.Expire,
-            IsActive:    true,
-            CreatedAt:   ptypes.TimestampNow(),
-            UpdatedAt:   ptypes.TimestampNow(),
-        }
+			err = r.db.Collection(constant.CollectionSavedCard).UpdateId(bson.ObjectIdHex(card.Id), card)
+		}
+	} else {
+		card = &entity.SavedCard{
+			Id:          bson.NewObjectId().Hex(),
+			Token:       req.Token,
+			ProjectId:   req.ProjectId,
+			MerchantId:  req.MerchantId,
+			MaskedPan:   req.MaskedPan,
+			CardHolder:  req.CardHolder,
+			RecurringId: req.RecurringId,
+			Expire:      req.Expire,
+			IsActive:    true,
+			CreatedAt:   ptypes.TimestampNow(),
+			UpdatedAt:   ptypes.TimestampNow(),
+		}
 
-        err = r.db.Collection(constant.CollectionSavedCard).Insert(card)
-    }
+		err = r.db.Collection(constant.CollectionSavedCard).Insert(card)
+	}
 
-    if err != nil {
-        r.logError(savedCardQueryErrorCreate, []interface{}{"error", err.Error(), "data", card})
-        return constant.ErrDatabase
-    }
+	if err != nil {
+		r.logError(savedCardQueryErrorCreate, []interface{}{"error", err.Error(), "data", card})
+		return constant.ErrDatabase
+	}
 
-    return nil
+	return nil
 }
 
 func (r *Repository) DeleteSavedCard(
-    ctx context.Context,
-    req *repository.FindByStringValue,
-    rsp *repository.Result,
+	ctx context.Context,
+	req *repository.FindByStringValue,
+	rsp *repository.Result,
 ) error {
-    query := bson.M{"is_active": false, "updated_at": time.Now()}
-    err := r.db.Collection(constant.CollectionSavedCard).UpdateId(bson.ObjectIdHex(req.Value), bson.M{"$set": query})
+	query := bson.M{"is_active": false, "updated_at": time.Now()}
+	err := r.db.Collection(constant.CollectionSavedCard).UpdateId(bson.ObjectIdHex(req.Value), bson.M{"$set": query})
 
-    if err != nil && err != mgo.ErrNotFound {
-        r.logError(savedCardQueryErrorDelete, []interface{}{"error", err.Error(), "_id", req.Value})
-        return constant.ErrDatabase
-    }
+	if err != nil && err != mgo.ErrNotFound {
+		r.logError(savedCardQueryErrorDelete, []interface{}{"error", err.Error(), "_id", req.Value})
+		return constant.ErrDatabase
+	}
 
-    return nil
+	return nil
 }
 
 func (r *Repository) FindSavedCards(
-    ctx context.Context,
-    req *repository.SavedCardRequest,
-    rsp *repository.SavedCardList,
+	ctx context.Context,
+	req *repository.SavedCardRequest,
+	rsp *repository.SavedCardList,
 ) error {
-    var cards []*entity.SavedCard
+	var cards []*entity.SavedCard
 
-    query := bson.M{"token": req.Token, "is_active": true}
-    err := r.db.Collection(constant.CollectionSavedCard).Find(query).All(&cards)
+	query := bson.M{"token": req.Token, "is_active": true}
+	err := r.db.Collection(constant.CollectionSavedCard).Find(query).All(&cards)
 
-    if err != nil && err != mgo.ErrNotFound {
-        r.logError(savedCardQueryErrorFind, []interface{}{"error", err.Error(), "query", query})
-        return nil
-    }
+	if err != nil && err != mgo.ErrNotFound {
+		r.logError(savedCardQueryErrorFind, []interface{}{"error", err.Error(), "query", query})
+		return nil
+	}
 
-    if len(cards) > 0 {
-        rsp.SavedCards = cards
-    }
+	if len(cards) > 0 {
+		rsp.SavedCards = cards
+	}
 
-    return nil
+	return nil
 }
 
 func (r *Repository) FindSavedCardById(
-    ctx context.Context,
-    req *repository.FindByStringValue,
-    rsp *entity.SavedCard,
+	ctx context.Context,
+	req *repository.FindByStringValue,
+	rsp *entity.SavedCard,
 ) error {
-    var card *entity.SavedCard
-    err := r.db.Collection(constant.CollectionSavedCard).FindId(bson.ObjectIdHex(req.Value)).One(&card)
+	var card *entity.SavedCard
+	err := r.db.Collection(constant.CollectionSavedCard).FindId(bson.ObjectIdHex(req.Value)).One(&card)
 
-    if err != nil {
-        if err == mgo.ErrNotFound {
-            return constant.ErrNotFound
-        }
+	if err != nil {
+		if err == mgo.ErrNotFound {
+			return constant.ErrNotFound
+		}
 
-        r.logError(savedCardQueryErrorFind, []interface{}{"error", err.Error(), "_id", req.Value})
-        return constant.ErrDatabase
-    }
+		r.logError(savedCardQueryErrorFind, []interface{}{"error", err.Error(), "_id", req.Value})
+		return constant.ErrDatabase
+	}
 
-    rsp.Id = card.Id
-    rsp.Token = card.Token
-    rsp.ProjectId = card.ProjectId
-    rsp.MerchantId = card.MerchantId
-    rsp.Expire = card.Expire
-    rsp.MaskedPan = card.MaskedPan
-    rsp.RecurringId = card.RecurringId
-    rsp.IsActive = card.IsActive
-    rsp.CreatedAt = card.CreatedAt
-    rsp.UpdatedAt = card.UpdatedAt
+	rsp.Id = card.Id
+	rsp.Token = card.Token
+	rsp.ProjectId = card.ProjectId
+	rsp.MerchantId = card.MerchantId
+	rsp.Expire = card.Expire
+	rsp.MaskedPan = card.MaskedPan
+	rsp.CardHolder = card.CardHolder
+	rsp.RecurringId = card.RecurringId
+	rsp.IsActive = card.IsActive
+	rsp.CreatedAt = card.CreatedAt
+	rsp.UpdatedAt = card.UpdatedAt
 
-    return nil
+	return nil
 }
 
 func (r *Repository) logError(msg string, data []interface{}) {
-    zap.S().Errorw(fmt.Sprintf("[PAYSUPER_REPOSITORY] %s", msg), data...)
+	zap.S().Errorw(fmt.Sprintf("[PAYSUPER_REPOSITORY] %s", msg), data...)
 }
--- pkg/proto/entity/encoder.go
+++ pkg/proto/entity/encoder.go
@@ -1,93 +1,96 @@
 package entity
 
 import (
-    "github.com/globalsign/mgo/bson"
-    "github.com/golang/protobuf/ptypes"
-    "time"
+	"github.com/globalsign/mgo/bson"
+	"github.com/golang/protobuf/ptypes"
+	"time"
 )
 
 type MgoExpire struct {
-    Month string `bson:"month" json:"month"`
-    Year  string `bson:"year" json:"year"`
+	Month string `bson:"month" json:"month"`
+	Year  string `bson:"year" json:"year"`
 }
 
 type MgoSavedCard struct {
-    Id          bson.ObjectId `bson:"_id"`
-    Token       string        `bson:"token"`
-    ProjectId   bson.ObjectId `bson:"project_id"`
-    MerchantId  bson.ObjectId `bson:"merchant_id"`
-    MaskedPan   string        `bson:"masked_pan"`
-    Expire      *MgoExpire    `bson:"expire"`
-    RecurringId string        `bson:"recurring_id"`
-    IsActive    bool          `bson:"is_active"`
-    CreatedAt   time.Time     `bson:"created_at"`
-    UpdatedAt   time.Time     `bson:"updated_at"`
+	Id          bson.ObjectId `bson:"_id"`
+	Token       string        `bson:"token"`
+	ProjectId   bson.ObjectId `bson:"project_id"`
+	MerchantId  bson.ObjectId `bson:"merchant_id"`
+	MaskedPan   string        `bson:"masked_pan"`
+	CardHolder  string        `bson:"card_holder"`
+	Expire      *MgoExpire    `bson:"expire"`
+	RecurringId string        `bson:"recurring_id"`
+	IsActive    bool          `bson:"is_active"`
+	CreatedAt   time.Time     `bson:"created_at"`
+	UpdatedAt   time.Time     `bson:"updated_at"`
 }
 
 func (m *SavedCard) GetBSON() (interface{}, error) {
-    st := &MgoSavedCard{
-        Id:         bson.ObjectIdHex(m.Id),
-        Token:      m.Token,
-        ProjectId:  bson.ObjectIdHex(m.ProjectId),
-        MerchantId: bson.ObjectIdHex(m.MerchantId),
-        MaskedPan:  m.MaskedPan,
-        Expire: &MgoExpire{
-            Month: m.Expire.Month,
-            Year:  m.Expire.Year,
-        },
-        RecurringId: m.RecurringId,
-        IsActive:    m.IsActive,
-    }
+	st := &MgoSavedCard{
+		Id:         bson.ObjectIdHex(m.Id),
+		Token:      m.Token,
+		ProjectId:  bson.ObjectIdHex(m.ProjectId),
+		MerchantId: bson.ObjectIdHex(m.MerchantId),
+		MaskedPan:  m.MaskedPan,
+		CardHolder: m.CardHolder,
+		Expire: &MgoExpire{
+			Month: m.Expire.Month,
+			Year:  m.Expire.Year,
+		},
+		RecurringId: m.RecurringId,
+		IsActive:    m.IsActive,
+	}
 
-    t, err := ptypes.Timestamp(m.CreatedAt)
+	t, err := ptypes.Timestamp(m.CreatedAt)
 
-    if err != nil {
-        return nil, err
-    }
+	if err != nil {
+		return nil, err
+	}
 
-    st.CreatedAt = t
+	st.CreatedAt = t
 
-    t, err = ptypes.Timestamp(m.UpdatedAt)
+	t, err = ptypes.Timestamp(m.UpdatedAt)
 
-    if err != nil {
-        return nil, err
-    }
+	if err != nil {
+		return nil, err
+	}
 
-    st.UpdatedAt = t
+	st.UpdatedAt = t
 
-    return st, nil
+	return st, nil
 }
 
 func (s *SavedCard) SetBSON(raw bson.Raw) error {
-    decoded := new(MgoSavedCard)
-    err := raw.Unmarshal(decoded)
+	decoded := new(MgoSavedCard)
+	err := raw.Unmarshal(decoded)
 
-    if err != nil {
-        return err
-    }
+	if err != nil {
+		return err
+	}
 
-    s.Id = decoded.Id.Hex()
-    s.Token = decoded.Token
-    s.ProjectId = decoded.ProjectId.Hex()
-    s.MerchantId = decoded.MerchantId.Hex()
-    s.MaskedPan = decoded.MaskedPan
-    s.Expire = &CardExpire{
-        Month: decoded.Expire.Month,
-        Year:  decoded.Expire.Year,
-    }
-    s.RecurringId = decoded.RecurringId
-    s.IsActive = decoded.IsActive
-    s.CreatedAt, err = ptypes.TimestampProto(decoded.CreatedAt)
+	s.Id = decoded.Id.Hex()
+	s.Token = decoded.Token
+	s.ProjectId = decoded.ProjectId.Hex()
+	s.MerchantId = decoded.MerchantId.Hex()
+	s.MaskedPan = decoded.MaskedPan
+	s.CardHolder = decoded.CardHolder
+	s.Expire = &CardExpire{
+		Month: decoded.Expire.Month,
+		Year:  decoded.Expire.Year,
+	}
+	s.RecurringId = decoded.RecurringId
+	s.IsActive = decoded.IsActive
+	s.CreatedAt, err = ptypes.TimestampProto(decoded.CreatedAt)
 
-    if err != nil {
-        return err
-    }
+	if err != nil {
+		return err
+	}
 
-    s.UpdatedAt, err = ptypes.TimestampProto(decoded.UpdatedAt)
+	s.UpdatedAt, err = ptypes.TimestampProto(decoded.UpdatedAt)
 
-    if err != nil {
-        return err
-    }
+	if err != nil {
+		return err
+	}
 
-    return nil
+	return nil
 }
