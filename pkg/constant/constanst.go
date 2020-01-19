52dce0599a4a6a921e13dd6c49b4a7ffd326d95f
Vadim Sabirov <pr0head@gmail.com>
2020-01-19 03:18:46 +0300

    feat: Move constants to the proto repo

--- pkg/constant/constanst.go
+++ pkg/constant/constanst.go
@@ -3,38 +3,12 @@ package constant
 import "errors"
 
 const (
-	PayOneTopicNotifyPaymentName  = "notify-payment"
-	PayOneTopicNotifyMerchantName = "notify-merchant"
-
 	TaxjarTransactionsTopicName      = "taxjar-sync-transactions"
 	TaxjarTransactionsRetryTopicName = "taxjar-sync-transactions-retry"
 	TaxjarRefundsTopicName           = "taxjar-sync-refunds"
 	TaxjarRefundsRetryTopicName      = "taxjar-sync-refunds-retry"
 	TaxjarNotificationStatusPayment  = "taxjar-payment"
 	TaxjarNotificationStatusRefund   = "taxjar-refund"
-
-	PaymentSystemCardPayDateFormat  = "2006-01-02T15:04:05Z"
-	PaymentSystemGroupAliasBankCard = "BANKCARD"
-	PaymentSystemGroupAliasWebMoney = "WEBMONEY"
-	PaymentSystemGroupAliasQiwi     = "QIWI"
-	PaymentSystemGroupAliasNeteller = "NETELLER"
-	PaymentSystemGroupAliasAlipay   = "ALIPAY"
-	PaymentSystemGroupAliasBitcoin  = "BITCOIN"
-
-	OrderStatusNew                         = 0
-	OrderStatusPaymentSystemCreate         = 1
-	OrderStatusPaymentSystemRejectOnCreate = 2
-	OrderStatusPaymentSystemReject         = 3
-	OrderStatusPaymentSystemComplete       = 4
-	OrderStatusProjectInProgress           = 5
-	OrderStatusProjectComplete             = 6
-	OrderStatusProjectPending              = 7
-	OrderStatusProjectReject               = 8
-	OrderStatusRefund                      = 9
-	OrderStatusChargeback                  = 10
-	OrderStatusPaymentSystemDeclined       = 11
-	OrderStatusPaymentSystemCanceled       = 12
-	OrderStatusItemReplaced                = 13
 
 	RegistryKubernetes = "kubernetes"
 
